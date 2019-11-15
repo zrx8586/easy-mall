7cbad8ce7d5acf71da2929ddd61e1a506d51a895
wanglong <lwang@leapstack.cn>
2019-11-15 14:23:40 +0800

    # init project

---
+++ src/main/java/com/leapstack/wanglong/easymall/EasyMallApplication.java
@@ -0,0 +1,13 @@
+package com.leapstack.wanglong.easymall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leapstack/wanglong/easymall/EasyMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leapstack.wanglong.easymall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
